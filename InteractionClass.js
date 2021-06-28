534d31196f70b8875f28ccd885ea63777d58803b
dragonizedpizza <dragon@dragonizedpizza.com>
2021-06-28 07:39:27 +0200

    Initial commit

---
+++ InteractionClass.js
@@ -0,0 +1,58 @@
+module.exports = class Interaction {
+  constructor(options, client) {
+    this.client = client;
+    this.type = options.type == 2 ? "APPLICATION_COMMAND" : null;
+    this.channel = this.client.channels.resolve(options.channel_id);
+    this.channelID = options.channel_id;
+    this.guild = this.client.guilds.cache.get(options.guild_id);
+    this.guildID = options.guild_id;
+    this.member = new Discord.GuildMember(
+      this.client,
+      options.member,
+      this.guild
+    );
+    this.authorID = options.member.user.id;
+    this.author = this.member.user;
+    this.args = options.data.options;
+    this.id = options.id;
+    this.token = options.token;
+    this.applicationID = options.applicationID;
+    this.replied = false;
+    this.deferred = false;
+  }
+  async reply(content, options) {
+    if (this.deferred || this.replied)
+      throw new Error("Interaction already replied.");
+    const apiMessage =
+      content instanceof Discord.APIMessage
+        ? content
+        : Discord.APIMessage.create(this, content, options);
+    const { data, files } = await apiMessage.resolveData().resolveFiles();
+
+    await this.client.api.interactions(this.id, this.token).callback.post({
+      data: {
+        type: "CHANNEL_MESSAGE_WITH_SOURCE",
+        data,
+      },
+      files,
+    });
+    this.replied = true;
+  }
+
+  async defer({ ephemeral } = {}) {
+    if (this.deferred || this.replied)
+      throw new Error("Interaction already replied.");
+    await this.client.api.interactions(this.id, this.token).callback.post({
+      data: {
+        type: "DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE",
+        data: {
+          flags: ephemeral ? 1 << 6 : undefined,
+        },
+      },
+    });
+    this.deferred = true;
+  }
+  isCommand() {
+    return !this.type;
+  }
+};
---
+++ index.js
@@ -0,0 +1,51 @@
+const Interaction = require("./interactionClass.js");
+module.exports = class SlashCommandHandler {
+  constructor(client) {
+    if (!client) throw new Error("No client provided.");
+    this.client = client;
+  }
+  add(slash = {}) {
+    if (!slash.name) throw new Error("Invalid name provided.");
+    if (!slash.description) throw new Error("Invalid description provided.");
+    this.client.api.applications(this.client.user.id).commands.post({
+      data: {
+        name: slash.name.toLowerCase(),
+        description: slash.description,
+        options: slash.options
+          ? slash.options.map((x) => {
+              const returnObj = x.options;
+              returnObj.name = returnObj.name.toLowerCase();
+              return returnObj;
+            })
+          : undefined,
+      },
+    });
+  }
+  bulkAdd(slashCmds) {
+    if (!slashCmds) throw new Error("Invalid commands provided.");
+    if (!Array.isArray(slashCmds))
+      throw new Error("Slash commands are not an array.");
+    await this.client.api.interactions(this.client.user.id).commands.put({
+      data: slashCmds.map((x) => {
+        return {
+          name: x.name.toLowerCase(),
+          description: x.description,
+          options: x.options
+            ? x.options.map((x) => {
+                const returnObj = x;
+                returnObj.name = returnObj.name.toLowerCase();
+                return returnObj;
+              })
+            : undefined,
+        };
+      }),
+    });
+  }
+  listen() {
+    this.client.ws.on("INTERACTION_CREATE", (rawInt) => {
+      const interaction = new Interaction(rawInt);
+      if (!interaction.isCommand()) return;
+      this.client.emit("slashCreate", interaction);
+    });
+  }
+};
